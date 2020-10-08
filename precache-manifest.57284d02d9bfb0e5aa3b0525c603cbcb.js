d9f0e2d93247d1b872fbb067587a1d4fe0703c27
Ahmad Iqbal Hafizi <ahmad.hafizi@tuhh.de>
2020-10-08 14:42:12 +0200

    Updates

---
+++ precache-manifest.57284d02d9bfb0e5aa3b0525c603cbcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3702a11f53a2af64864e5fac3c178c",
+    "url": "/jobsearch/index.html"
+  },
+  {
+    "revision": "137013427eed791427fa",
+    "url": "/jobsearch/static/css/main.a6e981f3.chunk.css"
+  },
+  {
+    "revision": "72b9a16e9b5e33230ed4",
+    "url": "/jobsearch/static/js/2.e2c7657d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jobsearch/static/js/2.e2c7657d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "137013427eed791427fa",
+    "url": "/jobsearch/static/js/main.e7cd5a04.chunk.js"
+  },
+  {
+    "revision": "f4baa1314b33eda07757",
+    "url": "/jobsearch/static/js/runtime-main.cc7fb850.js"
+  },
+  {
+    "revision": "fc568113ad72433c2e3802588369dd2a",
+    "url": "/jobsearch/static/media/backgroundImg.fc568113.png"
+  }
+]);
